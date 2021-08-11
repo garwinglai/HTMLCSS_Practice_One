1565496ff854e24ec555ba92e79fe859b9572d50
Garwing Lai <garwinglai@gmail.com>
2021-08-11 14:56:34 -0700

    changed res.send pathing

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname + '/public')))
 
 app.get('/', (req, res) => {
-  res.sendFile('public/index.html', { root: __dirname })
+  res.sendFile('index.html', { root: __dirname })
 })
 
 app.listen(3000, (req, res) => {
