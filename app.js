c66e8d7cf0e94c297601b279e5079a8460650d55
Garwing Lai <garwinglai@gmail.com>
2021-08-10 23:04:12 -0700

    path.join

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const path = require('path')
 
 const app = express();
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(path.join(__dirname + '/public')))
 
 app.get('/', (req, res) => {
   res.sendFile('index.html', { root: __dirname })
